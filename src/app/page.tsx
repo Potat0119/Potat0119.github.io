b25b3b09230647032d54569fd42178e90b7b8be6
Potat0119 <its.grobal01@gmail.com>
2024-11-02 01:20:05 +0900

    page.tsx reconfig. favicon changed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,34 @@ import Home from "./compornents/Home";
 import Footer from "./compornents/Footer";
 import HomeJP from "./compornents/HomeJP";
 
+
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const [content, setContent] = useState<"JPPage" | "ENPage" | null>(null);
-  const searchParams = useSearchParams().get('content');
+  const [content, setContent] = useState<"JPPage" | "ENPage">("ENPage"); // デフォルトを英語に設定
+
+  function SearchParamsComponent() {
+    const searchParams = useSearchParams().get('content');
+
+    useEffect(() => {
+      if (searchParams === "JPPage" || searchParams === "ENPage") {
+        setContent(searchParams);
+      }
+    }, [searchParams]);
+
+    return null; // 状態を更新するだけのコンポーネントなのでレンダリングは不要
+  }
 
   const toggleLanguage = () => {
     setContent(prevContent => (prevContent === "ENPage" ? "JPPage" : "ENPage"));
   };
 
   useEffect(() => {
-    if (searchParams === "JPPage" || searchParams === "ENPage") {
-      setContent(searchParams);
-    } else {
-      const userLanguage = navigator.language.startsWith('ja') ? 'JPPage' : 'ENPage';
-      setContent(userLanguage);
-    }
-
     const timeoutId = setTimeout(() => {
       setLoading(false);
     }, 1000);
 
     return () => clearTimeout(timeoutId);
-  }, [searchParams]);
+  }, []); // 初回のみ実行されるように依存配列を空に
 
   return (
     <div>
@@ -40,11 +45,12 @@ export default function App() {
         <div>
           <Header toggleLanguage={toggleLanguage} />
           <Suspense fallback={<LoadingScreen />}>
-            <div>
-              {content === 'ENPage' && <Home />}
-              {content === 'JPPage' && <HomeJP />}
-            </div>
+            <SearchParamsComponent />
           </Suspense>
+          <div>
+            {content === 'ENPage' && <Home />}
+            {content === 'JPPage' && <HomeJP />}
+          </div>
           <Footer />
         </div>
       )}

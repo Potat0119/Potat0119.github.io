73c586980be561da61e25adac61ffcc47e5a7102
Pot4t0 <its.grobal01@gmail.com>
2025-05-23 16:12:08 +0900

    Metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 export const metadata = {
   title: 'REO YOSHIOKA',
   description: 'REO YOSHIOKA Homepage',
+  keywords: ['portfolio', 'developer','texture creator'],
+  locale: 'ja_JP',
+  type: 'website',
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 import { Inter } from 'next/font/google'
- 
+
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
   subsets: ['latin'],
